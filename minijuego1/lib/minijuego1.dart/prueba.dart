2dec92935b6fe0a544bdd9d772dea4f64535b52c
Dmlsito <101351451+Dmlsito@users.noreply.github.com>
2023-01-27 08:38:04 +0100

    juyego

---
+++ minijuego1/lib/minijuego1.dart/prueba.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Prueba extends StatelessWidget {
+  @override
+  Widget build(Object context) {
+    return Scaffold(body: Center(child: Text("HOla que tal")));
+  }
+}
